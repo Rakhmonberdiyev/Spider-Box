f2189b2a79637e76ee98f8c234fe4eef7f721b10
RAximberdi <r.raxonberdiyev@newuu.uz>
2024-05-03 01:22:44 +0500

    first commit

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,15 @@
+import oqtepa from './oqtepa.jpg'
+import street from './street.jpg'
+import kfc from './kfc.jpg'
+import logo from './logo1.png'
+import search_icon from './search_icon.svg'
+import basket_icon from './basket_icon.svg'
+
+export const assets = {
+    oqtepa,
+    street,
+    kfc,
+    logo,
+    search_icon,
+    basket_icon
+}
